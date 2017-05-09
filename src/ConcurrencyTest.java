7f571e35c97937b7a45d4036e765bca0ecc2882b
arkan382382 <arkan382382@outlook.com>
2017-05-09 19:47:26 +0200

    Initial commit

---
+++ src/ConcurrencyTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by arkan on 09.05.2017.
+ *
+ * https://docs.oracle.com/javase/tutorial/essential/concurrency/procthread.html
+ * */
+public class ConcurrencyTest {
+    public static void main(String[] args){
+        
+    }
+}
